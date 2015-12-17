95415f9582a14a591514a7082d7be009a8b424c0
Devin Maiello <maielld1@tcnj.edu>
2015-12-17 16:54:09 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Devin Maiello");
  var awesomeThoughts="I am Devin and I am AWESOME";
  var funThoughts=awesomeThoughts.replace("AWESOME", "FUN");
   $("#main").append(funThoughts);
+  $("header").prepend(HTMLheaderName.replace("%data%", "Devin Maiello"));
+  $("header").prepend(HTMLheaderRole.replace("%data%", "Web Developer"));
+  
