dd6a185bcbadd1b9e08f36a354f1afd7c231278e
Devin Maiello <maielld1@tcnj.edu>
2015-12-17 16:44:51 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,3 +4,4 @@ This is empty on purpose! Your code to build the resume will go here.
  $("#main").append("Devin Maiello");
  var awesomeThoughts="I am Devin and I am AWESOME";
  var funThoughts=awesomeThoughts.replace("AWESOME", "FUN");
+  $("#main").append(funThoughts);
